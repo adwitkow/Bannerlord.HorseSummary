725fa65ba38a5302dafb359211653c3053f63416
adwitkow <adamadik@poczta.fm>
2026-06-23 22:01:20 +0200

    Add a line displaying herding ratio, refactor tooltip creation a bit

--- Bannerlord.HorseSummary/MountTooltip.cs
+++ Bannerlord.HorseSummary/MountTooltip.cs
@@ -11,6 +11,8 @@ namespace Bannerlord.HorseSummary;
 
 internal class MountTooltip
 {
+    private static readonly Color RedColor = new Color(0.82f, 0.12f, 0.07f, 1f);
+
     private static ItemCategory[] HorseCategories = [];
 
     public static List<TooltipProperty> Build()
@@ -39,46 +41,88 @@ internal class MountTooltip
 
         var result = new List<TooltipProperty>
         {
-            new TooltipProperty(new TextObject("{=Sb1MKbvP}Mounts and Pack Animals").ToString(), string.Empty, 0, false, TooltipProperty.TooltipPropertyFlags.Title),
+            CreateTitleLine(),
+            CreateMountedFootmenLine(party),
+            CreateHerdingLine(party),
+            CreateSeparatorLine()
         };
 
+        foreach (var (category, count) in horses)
+        {
+            result.Add(CreateCategoryLine(category, count));
+        }
+
+        result.Add(CreateTotalLine(totalHorses));
+
+        return result;
+    }
+
+    private static TooltipProperty CreateTotalLine(int totalHorses)
+    {
+        return new TooltipProperty(
+            new TextObject("{=kWVbHPtT}Total").ToString(),
+            totalHorses.ToString(),
+            0,
+            false,
+            TooltipProperty.TooltipPropertyFlags.RundownResult);
+    }
+
+    private static TooltipProperty CreateCategoryLine(ItemCategory category, int count)
+    {
+        return new TooltipProperty(
+            category.GetName().ToString(),
+            count.ToString(),
+            0);
+    }
+
+    private static TooltipProperty CreateTitleLine()
+    {
+        return new TooltipProperty(new TextObject("{=Sb1MKbvP}Mounts and Pack Animals").ToString(), string.Empty, 0, false, TooltipProperty.TooltipPropertyFlags.Title);
+    }
+
+    private static TooltipProperty CreateSeparatorLine()
+    {
+        return new TooltipProperty(
+            string.Empty,
+            string.Empty,
+            0,
+            false,
+            TooltipProperty.TooltipPropertyFlags.RundownSeperator);
+    }
+
+    private static TooltipProperty CreateMountedFootmenLine(MobileParty party)
+    {
         var needingMount = party.Party.NumberOfMenWithoutHorse;
         var availableMounts = party.ItemRoster.NumberOfMounts;
         var mounted = Math.Min(availableMounts, needingMount);
 
         TextObject mountedTroopTextObject = new TextObject($"{mounted}/{needingMount}");
-        Color color = mounted < needingMount
-            ? new Color(0.82f, 0.12f, 0.07f, 1f)
+        Color color = needingMount > mounted
+            ? RedColor
             : default;
 
-        result.Add(new TooltipProperty(
-                new TextObject("{=5bSWSaPl}Footmen on horses").ToString(),
-                mountedTroopTextObject.ToString(),
-                0,
-                color));
-
-        result.Add(new TooltipProperty(
-            string.Empty,
-            string.Empty,
+        return new TooltipProperty(
+            new TextObject("{=5bSWSaPl}Footmen on horses").ToString(),
+            mountedTroopTextObject.ToString(),
             0,
-            false,
-            TooltipProperty.TooltipPropertyFlags.RundownSeperator));
+            color);
+    }
 
-        foreach (var (category, count) in horses)
-        {
-            result.Add(new TooltipProperty(
-                category.GetName().ToString(),
-                count.ToString(),
-                0));
-        }
+    private static TooltipProperty CreateHerdingLine(MobileParty party)
+    {
+        var totalManCount = party.MemberRoster.TotalManCount;
+        var herdSize = party.ItemRoster.NumberOfPackAnimals
+            + party.ItemRoster.NumberOfLivestockAnimals;
 
-        result.Add(new TooltipProperty(
-            new TextObject("{=kWVbHPtT}Total").ToString(),
-            totalHorses.ToString(),
+        TextObject herdingTextObject = new TextObject($"{herdSize}/{totalManCount}");
+        Color color = herdSize > totalManCount
+            ? RedColor
+            : default;
+
+        return new TooltipProperty(
+            new TextObject("{=NhAMSaWU}Herding").ToString(),
+            herdingTextObject.ToString(),
             0,
-            false,
-            TooltipProperty.TooltipPropertyFlags.RundownResult));
-
-        return result;
+            color);
     }
 }
